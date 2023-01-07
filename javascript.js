2a41bfb07494986390198ad3aa6fdd26a05792b4
masahiroyusuke <115112450+xX-Mame-Xx@users.noreply.github.com>
2023-01-07 17:43:05 +0900

    変更だよ

--- javascript.js
+++ javascript.js
@@ -1,6 +1,24 @@
-$('#page-link a[href*="#"]').click(function () {
-    var elmHash = $(this).attr('href');
-	var pos = $(elmHash).offset().top-60;
-	$('body,html').animate({scrollTop: pos}, 500);
-	return false;
-});
+$(function () {
+    var headerHight = 70;
+    $('a[href^="#"]').click(function () {
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? "html" : href);
+      var position = target.offset().top - headerHight;
+      $("html, body").animate({ scrollTop: position }, 550, 'swing');
+      return false;
+    });
+  });
+
+  $(function () {
+    $(window).scroll(function () {
+      const windowHeight = $(window).height();
+      const scroll = $(window).scrollTop();
+  
+      $('.up').each(function () {
+        const targetPosition = $(this).offset().top;
+        if (scroll > targetPosition - windowHeight + 50) {
+          $(this).addClass("is-fadein");
+        }
+      });
+    });
+  });
---
+++ scrollToHome.js
@@ -0,0 +1,3 @@
+function scrollToHome() {
+    scrollTo(rect, pos);
+}
